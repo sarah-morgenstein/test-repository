1faf4270f444033ee0e47a668d45a3be7fbb9987
Sarah Morgenstein <sarah.morgenstein@tri-c.edu>
2016-02-16 19:15:29 -0500

    change title

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="pure-menu pure-menu-open pure-menu-horizontal">
-        <h1>Parma Tea</h1>
+        <h1>Parma Tea &amp; More</h1>
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="page1.php">Page 1</a></li>
